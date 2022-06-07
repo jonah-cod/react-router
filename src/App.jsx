dad0144229c761a5d748815ab8354f4ae5c181dc
jonathan mwaniki <idolearndaily.com>
2022-06-07 10:41:30 +0300

    created beginning template and styles for book keeping app to demonstrate react routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className="navbar">
+
+      </div>
+      <div className="container">
+        
+      </div>
     </div>
   );
 }
